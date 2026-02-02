b8fe52c4b4be82f7832bde229f99f3391904d55c
Darinka Loncar <149894872+darinkaloncar@users.noreply.github.com>
2026-02-02 22:54:39 +0100

    Update door light to publish records

--- simulation/components/dl.py
+++ simulation/components/dl.py
@@ -1,5 +1,8 @@
+import json
+from globals import batch, publish_counter, publish_limit, counter_lock, publish_event
+
 class DoorLight:
-    def __init__(self, settings):
+    def init(self, settings):
         self.settings = settings
         self.simulated = settings.get("simulated", True)
         self._state = False
@@ -8,16 +11,37 @@ class DoorLight:
             from sensors.dl import RealLED
             self.impl = RealLED(settings)
         else:
-            from simulators.dl import SimulationLED
+            from simulation.simulators.dl import SimulationLED
             self.impl = SimulationLED(settings)
+
+    def _publish_state(self):
+        global publish_counter, publish_limit
+
+        payload = {
+            "measurement": "LightState",
+            "simulated": self.settings["simulated"],
+            "runs_on": self.settings["runs_on"],
+            "name": self.settings["name"],
+            "value": int(self._state)
+        }
+
+        with counter_lock:
+            batch.append(("LightState", json.dumps(payload), 0, True))
+            publish_counter += 1
+            if publish_counter >= publish_limit:
+                publish_event.set()
 
     def on(self):
         self._state = True
-        if self.impl: self.impl.on()
+        if self.impl:
+            self.impl.on()
+        self._publish_state()
 
     def off(self):
         self._state = False
-        if self.impl: self.impl.off()
+        if self.impl:
+            self.impl.off()
+        self._publish_state()
 
     def toggle(self):
         if self._state:
