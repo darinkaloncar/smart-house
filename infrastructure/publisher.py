9bbf626c9a943fd2878a6dcec56ffaa2f4c44224
DusicaPesic <130140285+DusicaPesic@users.noreply.github.com>
2026-02-02 22:43:09 +0100

    Add infrastructure module

---
+++ infrastructure/publisher.py
@@ -0,0 +1,33 @@
+import json
+import threading
+import time
+from paho.mqtt import publish
+
+from globals import batch, publish_counter, publish_limit, counter_lock, publish_event
+
+HOSTNAME = "localhost"
+PORT = 1883
+
+def publisher_task():
+    global publish_counter, publish_limit
+
+    while True:
+        publish_event.wait()
+
+        with counter_lock:
+            local_copy = batch.copy()
+            batch.clear()
+            publish_counter = 0
+
+        if local_copy:
+            publish.multiple(local_copy, hostname=HOSTNAME, port=PORT)
+            print(f"[PUBLISHER] Published {len(local_copy)} values.")
+
+
+        publish_event.clear()
+
+
+def start_publisher_thread():
+    th = threading.Thread(target=publisher_task, daemon=True)
+    th.start()
+    return th
